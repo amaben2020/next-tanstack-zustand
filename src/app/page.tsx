30b8e0faacf25eaf691a6f65ae717f12a589b027
algomachine007 <benneth@bamble.io>
2023-11-10 16:31:04 +0100

    fixing zustand types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 "use client";
-import Toggle from "@/components/elements/toggle/Toggle";
+import bearStore from "@/store/bearsStore";
 
+type TBearsZustandState = {
+  bears: number;
+  increasePopulation: () => void;
+  decreasePopulation: () => void;
+};
 export default function Home() {
+  const bears = bearStore((state) => state);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {/* <Todo /> */}
@@ -10,7 +17,9 @@ export default function Home() {
 
       {/* <Form /> */}
 
-      {Toggle()}
+      {bears?.bears}
+      <button onClick={bears.increasePopulation}>+</button>
+      <button onClick={bears.decreasePopulation}>-</button>
     </main>
   );
 }
--- src/store/bearsStore.ts
+++ src/store/bearsStore.ts
@@ -1,10 +1,26 @@
 import { create } from "zustand";
 
-type TBearState = { bears: number };
+type TBearState = {
+  bears: number;
+  increasePopulation: () => void;
+  decreasePopulation: () => void;
+};
 
-const useStore = create((set) => ({
+const bearStore = create<TBearState>()((set) => ({
   bears: 0,
   increasePopulation: () =>
     set((state: TBearState) => ({ bears: state.bears + 1 })),
+  decreasePopulation: () =>
+    set((state: TBearState) => {
+      if (state.bears === 0) {
+        return {
+          bears: 0,
+        };
+      } else {
+        return { bears: state.bears - 1 };
+      }
+    }),
   removeAllBears: () => set({ bears: 0 }),
 }));
+
+export default bearStore;
