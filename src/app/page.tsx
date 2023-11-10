f0b1f706c4fec3a641f8eb56dd169576cf50f170
algomachine007 <benneth@bamble.io>
2023-11-10 21:27:01 +0100

    adding elete functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import bearStore from "@/store/bearsStore";
 import todoStore from "@/store/todoStore";
+import { useId, useState } from "react";
 
 type TBearsZustandState = {
   bears: number;
@@ -10,6 +11,9 @@ type TBearsZustandState = {
 export default function Home() {
   const bears = bearStore((state) => state);
   const todos = todoStore((state) => state);
+  const [title, setTitle] = useState("");
+  const [isCompleted, setIsCompleted] = useState(false);
+  const id = useId();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -23,18 +27,49 @@ export default function Home() {
       <button onClick={bears.increasePopulation}>+</button>
       <button onClick={bears.decreasePopulation}>-</button>
 
-      {todos.todos.map((todo) => (
-        <div key={todo.title}>{todo.title}</div>
+      {JSON.stringify(todos?.todos)}
+
+      {todos?.todos?.map((todo) => (
+        <div key={todo.title}>
+          {todo.title}
+          {todo.id} {"  "}
+          <button
+            className="mx-4 border px-5 py-2"
+            onClick={() =>
+              todos.editTodo(
+                {
+                  id: todo.id,
+                  title,
+                  completed: isCompleted,
+                },
+                todo.id,
+              )
+            }
+          >
+            Edit TODO
+          </button>
+          <button
+            className="mx-4 border px-5 py-2"
+            onClick={() => todos.deleteTodo(todo.id)}
+          >
+            Delete TODO
+          </button>
+        </div>
       ))}
 
       <div className="my-10 rounded-lg border">
-        <input type="text" />
+        <input type="text" onChange={(e) => setTitle(e.target.value)} />
+        <input
+          type="checkbox"
+          checked={isCompleted}
+          onChange={(e) => setIsCompleted(e.target.checked)}
+        />
         <button
           onClick={() =>
             todos.addTodo({
-              id: 1,
-              title: "algo Uzor",
-              completed: true,
+              id: Math.random(),
+              title,
+              completed: isCompleted,
             })
           }
         >
--- src/store/todoStore.ts
+++ src/store/todoStore.ts
@@ -2,11 +2,16 @@ import { create } from "zustand";
 import { devtools, persist } from "zustand/middleware";
 // set is a function that returns an object
 type TTodo = {
-  id: string | number;
+  id: number;
   title: string;
   completed: boolean;
 };
-type TTodos = { todos: TTodo[]; addTodo: (todo: TTodo) => void };
+type TTodos = {
+  todos: TTodo[];
+  addTodo: (todo: TTodo) => void;
+  editTodo: (todo: TTodo, id: number) => void;
+  deleteTodo: (id: number) => void;
+};
 
 const todoStore = create<TTodos>()(
   devtools(
@@ -17,6 +22,34 @@ const todoStore = create<TTodos>()(
           set((state: TTodos) => ({
             todos: [...state.todos, todo],
           })),
+        editTodo: (todo: TTodo, id: number) =>
+          set((state: TTodos) => {
+            const updatedTodo = state.todos.map((t) => {
+              if (todo.id === id) {
+                console.log(t);
+                return {
+                  id: todo.id,
+                  title: todo.title,
+                  completed: todo.completed,
+                };
+              } else {
+                return { ...t };
+              }
+            });
+
+            return {
+              todos: updatedTodo,
+            };
+          }),
+        // }),
+        deleteTodo: (id: number) =>
+          set((state) => {
+            const filteredTodo = state.todos.filter((todo) => todo.id !== id);
+
+            return {
+              todos: filteredTodo,
+            };
+          }),
       }),
       { name: "todoStore" },
     ),
