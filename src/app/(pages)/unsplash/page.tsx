5500012094d700c9a36351b34e44eea3c6e3c3e9
algomachine007 <benneth@izesan.com>
2024-01-12 22:03:03 +0100

    next image finish

--- src/app/(pages)/unsplash/page.tsx
+++ src/app/(pages)/unsplash/page.tsx
@@ -1,39 +1,63 @@
+//@ts-nocheck
 "use client";
 import Search from "@/components/elements/search";
 import axios from "axios";
-import { useEffect, useReducer, useState } from "react";
-
-const baseURL = `https://api.unsplash.com/search/photos?client_id=vCRa8wxs48KESXSylxE84-WoUzK6l8YwpKCdVVfLiBI&page=1&query=office`;
+import Image from "next/image";
+import { Reducer, useCallback, useEffect, useReducer } from "react";
+import useSWR from "swr";
 
 console.log(process.env.UNSPLASH_ACCESS_KEY);
-export type TAction = {
-  type: "SET_QUERY";
-  payload: string;
+type TMode = "idle" | "loading" | "error";
+
+type TInitialState = {
+  images: any[];
+  query: string;
+  total_pages: number;
+  page: number;
+  mode: TMode;
 };
+
+type TAction =
+  | { type: "SET_QUERY"; payload: string }
+  | { type: "SET_MODE"; payload: TMode }
+  | { type: "NEXT_PAGE" }
+  | { type: "SET_IMAGES"; payload: { images: any[]; total_pages: number } };
+
 const Unsplash = () => {
-  const [data, setData] = useState([]);
-
-  type TInitialState = {
-    images: Array<string>;
-    query: string;
-    total_pages: number;
-    page: number;
-  };
-
-  const INITIAL_STATE: TInitialState = {
+  const INITIAL_STATE = {
     images: [],
     query: "",
     total_pages: 1,
     page: 1,
+    mode: "loading",
   };
 
   const reducer = (state: TInitialState, action: TAction) => {
     switch (action.type) {
       case "SET_QUERY": {
         return {
-          ...state,
+          ...INITIAL_STATE,
           query: action.payload,
         };
+      }
+      case "SET_MODE": {
+        return {
+          ...state,
+          mode: action.payload,
+        };
+      }
+      case "NEXT_PAGE": {
+        return {
+          ...state,
+          page: state.page + 1,
+        };
+      }
+      case "SET_IMAGES": {
+        console.log(action.payload);
+        const { images, total_pages } = action.payload;
+        const newImages = [...state.images, ...images];
+        const mode = !newImages.length ? "no-results" : "idle";
+        return { ...state, images: newImages, total_pages, mode };
       }
       default: {
         return state;
@@ -41,18 +65,64 @@ const Unsplash = () => {
     }
   };
 
-  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+  const [state, dispatch] = useReducer<Reducer<TInitialState, TAction>>(
+    reducer,
+    INITIAL_STATE,
+  );
 
-  console.log(state);
+  console.log("STATE", state);
 
-  const fetchImages = async () => {
+  console.log(state?.query);
+
+  const baseURL = `https://api.unsplash.com/search/photos?client_id=vCRa8wxs48KESXSylxE84-WoUzK6l8YwpKCdVVfLiBI&page=${state.page}&query=${state.query}`;
+
+  const fetchImages = useCallback(async () => {
     const response = await axios.get(baseURL);
-    setData(response.data);
-  };
+    return response.data;
+  }, [baseURL]);
+
+  const {
+    data: images,
+    isLoading,
+    error,
+    isValidating,
+  } = useSWR("images", fetchImages);
+
+  const TOTAL_PAGES = images?.total_pages;
+  const IMAGE_RESULTS = images?.results;
+  console.log(IMAGE_RESULTS);
+
+  useEffect(() => {
+    if (isLoading) {
+      dispatch({
+        type: "SET_MODE",
+        payload: "loading",
+      });
+    }
+    if (TOTAL_PAGES) {
+      dispatch({
+        type: "SET_IMAGES",
+        payload: { images: IMAGE_RESULTS, total_pages: TOTAL_PAGES },
+      });
+      dispatch({
+        type: "SET_MODE",
+        payload: "idle",
+      });
+    }
+
+    if (error) {
+      dispatch({
+        type: "SET_MODE",
+        payload: "error",
+      });
+    }
+  }, [IMAGE_RESULTS, TOTAL_PAGES, error, isLoading]);
+
+  console.log(state.page);
 
   useEffect(() => {
     fetchImages();
-  }, []);
+  }, [fetchImages]);
 
   return (
     <div>
@@ -62,6 +132,17 @@ const Unsplash = () => {
 
         <Search dispatch={dispatch} value={state.query} />
       </div>
+      <button
+        className="p-4 border rounded-lg my-6"
+        onClick={() => dispatch({ type: "NEXT_PAGE" })}
+      >
+        Handle Next
+      </button>
+      {state?.images.map((res) => (
+        <>
+          <Image src={res.urls.regular} width={200} height={100} alt="" />
+        </>
+      ))}
     </div>
   );
 };
